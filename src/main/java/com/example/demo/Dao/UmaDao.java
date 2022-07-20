712210ab8719adf7a11c822273f12348c11213f5
tokuman0130 <tokuman_r@works-hi.co.jp>
2022-07-20 16:43:30 +0900

    編集画面の実装

--- src/main/java/com/example/demo/Dao/UmaDao.java
+++ src/main/java/com/example/demo/Dao/UmaDao.java
@@ -21,6 +21,14 @@ public class UmaDao {
 		db.update("INSERT INTO sample (name,sex,age,mother,father,win,lose) VALUES(?,?,?,?,?,?,?)",umaentity.getName(),umaentity.getSex(),umaentity.getAge(),umaentity.getMother(),umaentity.getFather(),umaentity.getWin() ,umaentity.getLose() );
 	}
 	
+	//削除(DELETE)
+	public void deleteDb(Long id) {
+		//コンソールに表示
+		System.out.println("削除しました");
+		//DBからデータを削除
+		db.update("delete from sample where id=?", id);
+	}
+	
 	public List<UmaEntity> searchDb(){
 		String sql = "SELECT * FROM sample";
 
@@ -54,6 +62,48 @@ public class UmaDao {
 
 		//Controllerに渡す
 		return resultDb2;
+	}
+	
+//	kokokara
+	
+	public List<UmaEntity> selectOne(Long id) {
+
+		// コンソールに表示
+		System.out.println("編集画面を出します");
+		// データベースから目的の1件を取り出して、そのままresultDB1に入れる
+		List<Map<String, Object>> resultDb1 = db.queryForList("SELECT * FROM sample where id=?", id);
+		// 画面に表示しやすい形のList(resultDB2)を用意
+		List<UmaEntity> resultDb2 = new ArrayList<UmaEntity>();
+
+		// 1件ずつピックアップ
+		for (Map<String, Object> result1 : resultDb1) {
+			// データ1件分を1つのまとまりとするので、UmaEntity型の「UmaEntitydb」を生成
+			UmaEntity UmaEntitydb = new UmaEntity();
+			// id、nameのデータをUmaEntitydbに移す
+			UmaEntitydb.setId((int) result1.get("id"));
+			UmaEntitydb.setName((String) result1.get("name"));
+			UmaEntitydb.setSex((int) result1.get("sex"));
+			UmaEntitydb.setAge((int) result1.get("age"));
+			UmaEntitydb.setMother((String) result1.get("mother"));
+			UmaEntitydb.setFather((String) result1.get("father"));
+			UmaEntitydb.setWin((int) result1.get("win"));
+			UmaEntitydb.setLose((int) result1.get("lose"));
+			
+			// 移し替えたデータを持ったUmaEntitydbを、resultDB2に入れる
+			resultDb2.add(UmaEntitydb);
+		}
+
+		// Controllerに渡す
+		return resultDb2;
+	}
+	
+
+	// 更新の実行(UPDATE)
+	public void updateDb(Long id, UmaEntity umaentity) {
+		// コンソールに表示
+		System.out.println("編集の実行");
+		// UPDATEを実行
+		db.update("UPDATE sample SET name = ?, sex=?,age=?, mother=?, father=?, win=?,lose=? WHERE id = ?", umaentity.getName(),umaentity.getSex(),umaentity.getAge(),umaentity.getMother(),umaentity.getFather(),umaentity.getWin() ,umaentity.getLose(), id);
 	}
 	
 
--- src/main/java/com/example/demo/UmaController.java
+++ src/main/java/com/example/demo/UmaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.Dao.UmaDao;
@@ -47,5 +48,41 @@ public class UmaController {
 		
 		return "redirect:/view";
 	}
+	
+//	更新画面の表示
+	@RequestMapping("/edit/{id}")
+	public String editView(@PathVariable Long id, Model model) {
+
+		//DBからデータを1件取ってくる(リストの形)
+		List<UmaEntity> list = umadao.selectOne(id);
+
+		//リストから、オブジェクトだけをピックアップ
+		UmaEntity UmaEntitydb = list.get(0);
+
+		//スタンバイしているViewに向かって、データを投げる
+		model.addAttribute("umainput", UmaEntitydb);
+		model.addAttribute("title", "編集ページ");
+		return "form/edit";
+	}
+	
+	//更新処理(UPDATE)
+		@RequestMapping("/edit/{id}/exe")
+		public String editExe(@PathVariable Long id, Model model, Umainput umainput) {
+			//フォームの値をエンティティに入れ直し
+			UmaEntity UmaEntity = new UmaEntity();
+			System.out.println(umainput.getName());//取得できているかの確認
+			UmaEntity.setName(umainput.getName());
+			UmaEntity.setAge(umainput.getAge());
+			UmaEntity.setMother(umainput.getMother());
+			UmaEntity.setFather(umainput.getFather());
+			UmaEntity.setWin(umainput.getWin());
+			UmaEntity.setLose(umainput.getLose());
+			//更新の実行
+			umadao.updateDb(id,UmaEntity);
+			//一覧画面へリダイレクト
+			return "redirect:/view";
+		}
+		
+
 
 }
